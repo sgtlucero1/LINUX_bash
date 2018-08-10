3b2d5421b05098179ced90ca61ed1a6953e8a443
sgtlucero1 <42277016+sgtlucero1@users.noreply.github.com>
2018-08-10 10:28:29 -0700

    Create remove_user.sh

---
+++ remove_user.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+for i in `cat list | awk '{print $2}'`                                          
+do
+
+sshpass -p 'MYPASSWORD' ssh -o "StrictHostKeyChecking no" -o ConnectTimeout=5 -t  elucero@$i '
+echo 'MYPASSWORD' | sudo -S /usr/sbin/userdel -r USERNAME'
+
+EC=`echo $?`
+if [[ $EC == "0" ]];then
+MSG="Login OK"
+else
+MSG="Login Failed"
+fi
+echo -e "Checking $H,$i,ec=$EC,$MSG"
+done
