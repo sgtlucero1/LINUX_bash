20e0748902e9c3fe2a24b635c2ee1589e71686d2
sgtlucero1 <42277016+sgtlucero1@users.noreply.github.com>
2018-08-10 10:23:59 -0700

    Create chage.sh

---
+++ chage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash  
+#This script is to set password expiry to max days
+for i in `cat list | awk '{print $2}'`                                          
+do
+#MYPASSWORD refers to my own password 
+#USERNAME1 is my username. USERNAME2 is the username of the account you are attempting to unlock
+sshpass -p 'MYPASSWORD' ssh -o "StrictHostKeyChecking no" -o ConnectTimeout=5 -t  USERNAME1@$i '
+echo 'MYPASSWORD' | sudo -S /usr/bin/chage -M -1 USERNAME2'
+ EC=`echo $?`
+if [[ $EC == "0" ]];then
+ MSG="Login OK"
+ else
+ MSG="Login Failed"
+ fi
+ echo -e "Checking $H,$i,ec=$EC,$MSG"
+ done
