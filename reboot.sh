a2d55430bad5ac47a85a6b2509ff5b11ecdb6d13
sgtlucero1 <42277016+sgtlucero1@users.noreply.github.com>
2018-08-11 23:39:58 -0700

    Update reboot.sh

--- reboot.sh
+++ reboot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for i in `cat list | awk '{print $2}'`
 do
-sshpass -p 'CHuzwuza11??' ssh -o "StrictHostKeyChecking no" -o ConnectTimeout=5 -t elucero@$i "echo 'CHuzwuza11??' | sudo -S reboot"
+sshpass -p 'PASSWORD' ssh -o "StrictHostKeyChecking no" -o ConnectTimeout=5 -t elucero@$i "echo 'PASSWORD' | sudo -S reboot"
 
 EC=`echo $?`
 if [[ $EC == "0" ]];then
