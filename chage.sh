068df5e6c04504f58c3225e903e725ea5f6d10d5
sgtlucero1 <42277016+sgtlucero1@users.noreply.github.com>
2018-11-05 09:34:50 -0700

    Update chage.sh

--- chage.sh
+++ chage.sh
@@ -5,7 +5,7 @@ do
 #MYPASSWORD refers to my own password 
 #USERNAME1 is my username. USERNAME2 is the username of the account you are attempting to unlock
 sshpass -p 'MYPASSWORD' ssh -o "StrictHostKeyChecking no" -o ConnectTimeout=5 -t  USERNAME1@$i '
-echo 'MYPASSWORD' | sudo -S /usr/bin/chage -M -1 USERNAME2'
+echo 'MYPASSWORD' | sudo -S /usr/bin/chage -I -1 -m 0 -M 99999 -E -1 USERNAME2'
  EC=`echo $?`
 if [[ $EC == "0" ]];then
  MSG="Login OK"
